3cc0476c11f480f7b69619309f24dccc81750218
Laura Augusta <laurah.algusta@gmail.com>
2024-05-07 21:53:34 -0300

    feat-step-1

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view),
+    path("tweets/", views.tweet_list_view),
     path("tweets/<int:tweet_id>/", views.tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<p>Hello world<p/>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    QuerySet = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content":x.content} for x in QuerySet]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    # print(tweet_id)
-    obj = Tweet.objects.get(id = tweet_id)
-    return HttpResponse(f"<p>New tweet: {obj.content}<p/>")
+    """
+    REST API VIEW
+    """
+    data ={
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id = tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not found"
+        status = 404
+    
+    return JsonResponse(data, status=status)
