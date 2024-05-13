329041f75ff1048db8744379ba8988d80153192a
Laura Augusta <laurah.algusta@gmail.com>
2024-05-13 20:09:38 -0300

    feat-step-5

--- tweets/models.py
+++ tweets/models.py
@@ -4,3 +4,13 @@ class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "content": self.content, 
+            "likes": 12
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -6,12 +6,17 @@ from .models import Tweet
 from .forms import TweetForm
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
 # Create your views here.
+
+
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
@@ -22,35 +27,41 @@ def tweet_create_view(request, *args, **kwargs):
 
         if is_ajax(request=request):
             print("This is ajax")
-            return JsonResponse({}, status=201)
+            return JsonResponse(obj.serialize(), status=201)
 
         if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
-        
+    if form.erros:
+        if is_ajax(request=request):
+            return JsonResponse(form.errors, status=400)
+
     return render(request, "components/forms.html", context={"form": form})
+
 
 def tweet_list_view(request, *args, **kwargs):
     QuerySet = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content":x.content, "likes":12} for x in QuerySet]
+    tweets_list = [x.serialize() for x in QuerySet]
     data = {
+        "isUser": False,
         "response": tweets_list
     }
     return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
     """
-    data ={
+    data = {
         "id": tweet_id,
     }
     status = 200
     try:
-        obj = Tweet.objects.get(id = tweet_id)
+        obj = Tweet.objects.get(id=tweet_id)
         data["content"] = obj.content
     except:
         data["message"] = "Not found"
         status = 404
-    
+
     return JsonResponse(data, status=status)
