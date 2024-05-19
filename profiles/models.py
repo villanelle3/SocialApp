476e151f4c0c785b80c9583e358afc4b1f368468
Laura Augusta <laurah.algusta@gmail.com>
2024-05-18 22:51:05 -0300

    models update

--- profiles/models.py
+++ profiles/models.py
@@ -4,10 +4,19 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(User, related_name="following", blank=True)
 
 def user_did_save(sender, instance, created, *args, **kargs):
     if created:
